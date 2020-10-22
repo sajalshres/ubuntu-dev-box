04a2dc0e73ffa281177a1434751bf1f8444062b2
Sajal Shrestha <sajal.shrestha@cotiviti.com>
2020-10-22 20:29:50 +0545

    switched to packer

--- provision/kubernetes.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set +ex
-
-# Install latest MicroK8s
-# MicroK8s is packaged as a snap which requires snapd to be installed.
-# The latest Ubuntu release comes with this already built in.
-snap install microk8s --classic
